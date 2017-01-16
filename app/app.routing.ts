a8402a818adb7c03126188c6b891d6435afc24c0
graemeNorman <graemenorman@tengroup.com>
2017-01-16 12:25:27 +0000

    Second component added - about page and linked to app module and navigation

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,9 +2,11 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component'; //import home components
+import { AboutComponent } from './about/about.component'; //import home components
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: '', component: HomeComponent, pathMatch: 'full'} // redirect to home page on load
 ];
 
