0cc08f564dbfda0df5a0d554bdf3c9c6ee59fddf
graemeNorman <graemenorman@tengroup.com>
2017-01-17 10:45:23 +0000

    About component and template updated to work with demo grid in SASS. Old styles.css removed

--- app/about/about.component.ts
+++ app/about/about.component.ts
@@ -2,11 +2,11 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-about',
-    template: `<h1>{{welcome}}</h1>`
+    templateUrl: './app/about/about-template.html'
 })
 export class AboutComponent {
     welcome : string;
     constructor(){
-        this.welcome = "Welcome to about page"
+        this.welcome = "Template Demo 2"
     };
 };
