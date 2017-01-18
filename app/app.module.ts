20f61ec8a44af0261f8f033c78d4baba9c834c05
graemeNorman <graemenorman@tengroup.com>
2017-01-18 09:17:23 +0000

    Remove references to angular material as not using this anymore

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,21 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { testDirective } from './directives/form.directive';
+import { CardsComponent } from './cards/cards.component';
 import { routing } from "./app.routing";
 
-// Material Design Imports
-import { MdCardModule } from '@angular2-material/card';
-import { MdButtonModule } from '@angular2-material/button';
-import { MdIconModule } from '@angular2-material/icon';
-import { MdIconRegistry } from '@angular2-material/icon';
+// Directives
 import { testDirective } from "./directives/form.directive";
 
 @NgModule({
-  imports:      [ BrowserModule, routing, MdCardModule, MdButtonModule, MdIconModule ], //other modules the app depends on
-  declarations: [ AppComponent, HomeComponent, AboutComponent, testDirective ], // declare all directives and components
+  imports:      [ BrowserModule, routing ], //other modules the app depends on
+  declarations: [ AppComponent, HomeComponent, AboutComponent, CardsComponent, testDirective ], // declare all directives and components
   bootstrap : [ AppComponent ], // root component to bootstrap
-  providers: [ MdIconRegistry ]
 })
 
 export class AppModule { }
