8702ae0cc86b32897482b48aef9c30c272618b72
graemeNorman <graemenorman@tengroup.com>
2017-02-02 14:36:22 +0000

    structure and naming changes for settings folder

---
+++ app/settings/brands/coutts/settings.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+/***
+ * MODULE SWITCHES
+***/
+export var mod_inspiration = true;
+export var mod_dining = true;
+export var mod_entertainment = false;
+export var mod_travel = true;
+export var mod_events = false;
+export var mod_home = false;
+
+
+export var travel_layout = 'default'
---
+++ app/settings/brands/ten/settings.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+/***
+ * MODULE SWITCHES
+***/
+//export var mod_inspiration = true;
+export var mod_dining = true;
+export var mod_entertainment = true;
+export var mod_travel = true;
+export var mod_events = true;
+export var mod_home = true;
