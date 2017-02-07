8cd962c4df5f7081522900dcc19d7ecc25f6dfe1
Simone Sabbatini <simonesabbatini@tengroup.com>
2017-02-07 17:24:10 +0000

    - added gulp plugin to load the json file from an external API - added gulp plugin to turn a JSON file into a LESS file

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,12 @@ var gulp        = require('gulp'),
     rename      = require('gulp-rename'),
     es          = require('event-stream'),
     path        = require('path'),
-    less = require('gulp-less');
+    less        = require('gulp-less');
 // minifyCss   = require('gulp-minify-css'),
 // concat      = require('gulp-concat'),
+
+var gulpJson = require('gulp-json');
+var jsonCss = require('gulp-json-css');
 
 /* SASS DIRECTORY */
 var LESS_SRC = ['assets/less/**/**/*.less', 'app/**/*.less'];
@@ -31,7 +34,6 @@ gulp.src(LESS_SRC)
     .pipe(gulp.dest('./css/'))
 });
 
-
 /*
     Watchers
 */
@@ -39,4 +41,20 @@ gulp.task('watch', ['watch:less']);
 gulp.task('watch:less', function () {
     gulp.watch(LESS_SRC, ['concatMinify:less']);
 });
+
+gulp.task('api', function () {
+    gulp.src('api.json')
+      .pipe(gulpJson())
+      .pipe(gulp.dest('app/config'));
+});
+
+gulp.task('test', function() {
+  return gulp
+    .src(['app/config/theme.json'])
+    .pipe(jsonCss({targetPre: 'less'}))
+    .pipe(gulp.dest('app/config/'));
+});
+
+
+
 
