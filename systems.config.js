225cee4ec949cd5eeb3398d32ba0d6c81503e4a0
graemeNorman <graemenorman@tengroup.com>
2017-02-07 17:50:53 +0000

    File updates to be able to load all dependencies before the app bootstraps, this includes adding a loading page for the user while waiting

--- systems.config.js
+++ systems.config.js
@@ -1,44 +1,104 @@
-/**
- * System configuration for Angular samples
- * Adjust as necessary for your application needs.
- */
-(function (global) {
-  System.config({
-    paths: {
-      // paths serve as alias
-      'npm:': 'node_modules/'
-    },
-    // map tells the System loader where to look for things
-    map: {
-      // our app is within the app folder
-      app: 'app',
-      // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      // other libraries
-      'rxjs':                       'npm:rxjs',
-      'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
-    },
-    // packages tells the System loader how to load when no filename and/or no extension
-    packages: {
-      app: {
-        main: './transpiled-js/main.js', // path to main.js
-        format: 'cjs',  // commonjs
-        defaultExtension: 'js'
-      },
-      rxjs: {
-        defaultExtension: 'js'
-      },
-      'angular2-in-memory-web-api': {
-        main: './index.js',
-        defaultExtension: 'js'
-      }
-    }
-  });
-})(this);
+// /**
+//  * System configuration for Angular samples
+//  * Adjust as necessary for your application needs.
+//  */
+(function( global ) {
+
+	// Alias the path to the common rc1 vendor scripts.
+	var paths = {
+		'npm:': './node_modules/'
+	};
+
+	// Tell Angular how normalize path and package aliases.
+	var  map = {
+		app: 'app',
+		"@angular": "npm:/@angular",
+		"plugin-typescript": "npm:plugin-typescript/lib/plugin.js",
+		"rxjs": "npm:rxjs",
+		"tsconfig.json": "./tsconfig.json",
+		"typescript": "npm:typescript"
+	};
+
+	// Setup meta data for individual areas of the application.
+	var packages = {
+		"app": {
+          main: './transpiled-js/main.js', // path to main.js
+          format: 'cjs',  // commonjs
+          defaultExtension: 'js'
+		},
+		"rxjs": {
+			defaultExtension: 'js'
+		},
+		"angular-2-in-memory-web-api": {
+          main: './index.js',
+          defaultExtension: 'js'
+		}
+	};
+
+	var ngPackageNames = [
+		"common",
+		"compiler",
+		"core",
+		"http",
+		"platform-browser",
+		"platform-browser-dynamic",
+		"router",
+		"upgrade",
+	];
+
+	ngPackageNames.forEach(
+		function iterator( packageName ) {
+
+			var filename = ( "/bundles/" + packageName + ".umd.js" );
+
+			var ngPackage = packages[ "@angular/" + packageName ] = {
+				main: filename,
+				meta: {}
+			};
+
+			ngPackage.meta[ filename ] = {
+				typings: ( packageName + "/index.d.ts" )
+			};
+
+		}
+	);
+
+	System.config({
+		paths: paths,
+		map: map,
+		packages: packages,
+		transpiler: "plugin-typescript",
+		typescriptOptions: {
+			tsconfig: true
+		},
+		meta: {
+			typescript: {
+				exports: "ts"
+			}
+		}
+	});
+
+	// Load "./app/main.ts" (gets full path from package configuration above).
+	// --
+	// NOTE: We are attaching the resultant promise to the global scope so that other
+	// scripts may listen for the successful loading of the application.
+	global.bootstrapping = System
+		.import( "app" )
+		.then(
+			function handleResolve() {
+
+				console.info( "System.js successfully bootstrapped app." );
+
+			},
+			function handleReject( error ) {
+
+				console.warn( "System.js could not bootstrap the app." );
+				console.error( error );
+
+				return( Promise.reject( error ) );
+
+			}
+		)
+	;
+
+})( window );
