20efdd26b1714871bf5aba0d57eccae973176e21
Simone Sabbatini <simonesabbatini@tengroup.com>
2017-02-08 11:14:29 +0000

    added some helpers function to use in gulp

---
+++ helper.js
@@ -0,0 +1,12 @@
+var test = 'myVar';
+
+function buildThemeApi(site) {
+    return "http://demo7313901.mockable.io/"+site+"/theme";
+}
+
+function buildSettingApi(site) {
+    return "http://demo7313901.mockable.io/"+site+"/settings";
+}
+
+exports.buildThemeApi = buildThemeApi;
+exports.buildSettingApi = buildSettingApi;
